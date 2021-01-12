0159fb4fbcf8f9dacda8e4d0b6cba40593db357b
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-12 14:43:38 +0100

    Exo18

--- exercice17/js/main.js
+++ exercice17/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("button").click(function () {
         $("#text").show()
+        $("#exercise").append(`La méthode "show" correspond à la règle CSS "display : block" pour révéler un block.`)
     })
 })
 
---
+++ exercice18/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $("#text").hide()
+        $("#exercise").append(`La méthode "hide" correspond à la règle CSS "display : none" pour cacher un block.`)
+    })
+})
+
+// La méthode "hide" correspond à la règle CSS "display : none" pour cacher un block.
