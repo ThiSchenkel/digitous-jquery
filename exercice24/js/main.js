4543965984029f9a32cbcc1c9c4772378bb76b29
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-12 19:37:06 +0100

    Exo27

--- exercice24/js/main.js
+++ exercice24/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("button").click(function () {
-        $("#exercise #bg").animate({
+        $("#bg").animate({
             right: "100px",
         })
     })
