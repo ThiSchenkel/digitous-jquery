422585f54aa2d8f7d8ba455ffe033535b1a035f6
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-12 14:33:48 +0100

    Exo17

---
+++ exercice17/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $("#text").show()
+    })
+})
+
+// La méthode "show" correspond à la règle CSS "display : block" pour révéler un block.
