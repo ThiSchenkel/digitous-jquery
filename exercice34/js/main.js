9a5c35d7bce7f5ba72cab6e0cebdea2ea69b7111
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-14 10:09:02 +0100

    Exo34 avec correction

--- exercice34/js/main.js
+++ exercice34/js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
     $("button").click(function () {
-        console.log($("input").val());
         $.ajax({
-            url: "https://restcountries.eu/rest/v2/name/usa",
+            url: "https://restcountries.eu/rest/v2/name/" + $("input").val(), // ou ${$("input").val()}
             success: function (data, status, response) {
                 $("#country").html(`${data[0].name}`)
                 $("#capital").html(`${data[0].capital}`)
